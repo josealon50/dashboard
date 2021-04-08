2f23ada40481ad1974cc170c9772f990890fb2c3
Jose Alonso Leon <jleon@sandiegobloodbank.org>
2021-04-08 15:39:51 -0700

    Remove footer css unnecessary and added year to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import "./Footer.css";
 
 
 const footer = ( props ) => (
     <Navbar className='color-nav container-fluid' variant="dark" fixed="bottom" expand="lg">
-      <Navbar.Brand className='ml-auto'>SDBB 2021</Navbar.Brand>
+      <Navbar.Brand className='ml-auto'>SDBB { (new Date().getFullYear()) }</Navbar.Brand>
     </Navbar>
 );
 
